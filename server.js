152f83e53ca8891fa3d08f4d46bcc75e913be8fa
Aamir Yousuf <aamir.yousuf@honeywell.com>
2019-11-07 17:03:07 -0600

    Enable CORS.

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ var bodyParser = require('body-parser');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var obj = {
     "colors": [
