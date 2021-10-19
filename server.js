cdb85656004101732b7144bab36c3243d3bd5642
211054 <Aamir.Yousuf@resideo.com>
2021-10-19 16:57:23 -0500

    apple app site association

--- server.js
+++ server.js
@@ -185,6 +185,9 @@ app.get("/api/apex4", function (req, res) {
 app.get("/.well-known/assetlinks.json", function (req, res) {
     res.sendFile('./.well-known/assetlinks.json', { root: __dirname });
 });
+app.get("/apple-app-site-association", function (req, res) {
+    res.sendFile('./apple-app-site-association', { root: __dirname });
+});
 
 app.get('/emit', (req, res) => {
     console.log('REQUEST', req.body);
