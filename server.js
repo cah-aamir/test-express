ad34eb0c10bc23442ac7b0851d26f6f0e2c3303e
211054 <Aamir.Yousuf@resideo.com>
2021-10-25 14:54:46 -0500

    transfer apple applinks file to .well-known folder

--- server.js
+++ server.js
@@ -185,8 +185,8 @@ app.get("/api/apex4", function (req, res) {
 app.get("/.well-known/assetlinks.json", function (req, res) {
     res.sendFile('./.well-known/assetlinks.json', { root: __dirname });
 });
-app.get("/apple-app-site-association", function (req, res) {
-    res.sendFile('apple-app-site-association', { root: __dirname });
+app.get("/.well-known/apple-app-site-association", function (req, res) {
+    res.sendFile('./.well-known/apple-app-site-association', { root: __dirname });
 });
 
 app.get('/emit', (req, res) => {
